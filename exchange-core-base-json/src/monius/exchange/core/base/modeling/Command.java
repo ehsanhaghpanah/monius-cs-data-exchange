b9b170e1235e50269902387f55075d76385ac09c
Ehsan Haghpanah <anonim.developer@gmail.com>
2021-10-14 10:44:28 +0330

    beginning of the universe!

---
+++ exchange-core-base-json/src/monius/exchange/core/base/modeling/Command.java
@@ -0,0 +1,11 @@
+/**
+ * @author Ehsan Haghpanah (haghpanah@monius.net)
+ * Copyright (C) moniüs, 2013.
+ * All rights reserved.
+ */
+
+package monius.exchange.core.base.modeling;
+
+public class Command
+{
+}
